dd5bb8691dbb38c0d9c42771a6a455ba26b350cd
948190 <142780006+948190@users.noreply.github.com>
2025-04-14 11:56:56 +0530

    Update App.java

--- src/main/java/com/example/helloworld/App.java
+++ src/main/java/com/example/helloworld/App.java
@@ -3,5 +3,6 @@ package com.example.helloworld;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Azure DevOps!");
+        System.out.println("Hello Azure DevOps!");
     }
 }
